a69a5fb6f3d9c9eee23e2f76a507656c66073fad
Mikhail Yumanov <yumanov_m@mail.ru>
2020-11-24 17:21:23 +0300

    Added Specification::getUrl_map.

--- jsyntrax/src/main/java/org/atpfivt/jsyntrax/groovy_parser/Parser.groovy
+++ jsyntrax/src/main/java/org/atpfivt/jsyntrax/groovy_parser/Parser.groovy
@@ -1,17 +1,16 @@
 package org.atpfivt.jsyntrax.groovy_parser
 
+import org.atpfivt.jsyntrax.Specification
 import org.atpfivt.jsyntrax.units.tracks.Track
 import org.codehaus.groovy.control.CompilerConfiguration
 
 class Parser {
-    static Track parse(File file) {
+    static Specification parse(File file) {
         def config = new CompilerConfiguration()
         config.setScriptBaseClass(SyntraxScript.class.name)
         def shell = new GroovyShell(this.class.classLoader, new Binding(), config)
         shell.setVariable("None", null)
-        println "None is null? " + (shell.getVariable("None") == null)
-        println 'None is "null"? ' + (shell.getVariable("None") == "null")
 
-        return shell.evaluate(file) as Track
+        return new Specification(shell.evaluate(file) as Track, new HashMap<String, URL>())
     }
 }
