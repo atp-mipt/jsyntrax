1b1723cd84ae5e60e36adf3ec27d406e380f5f1f
Mikhail Yumanov <yumanov_m@mail.ru>
2020-11-24 17:03:38 +0300

    Added Node::getLink

--- src/main/java/com/syntrax/units/nodes/Node.java
+++ src/main/java/com/syntrax/units/nodes/Node.java
@@ -4,17 +4,25 @@ import com.syntrax.styles.Style;
 import com.syntrax.units.Unit;
 import com.syntrax.visitors.Visitor;
 
+import java.net.URL;
+
 public class Node implements Unit {
   Style style;
   String text;
+  URL link;
 
   public Node(String text) {
     this.text = text;
   }
 
-  public Node(Style style, String text) {
+  public Node(Style style, String text, URL link) {
     this.style = style;
     this.text = text;
+    this.link = link;
+  }
+
+  public URL getLink() {
+    return link;
   }
 
   @Override
