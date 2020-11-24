a9ae122b1bbda920cbb0112ffa015e472db620ab
Mikhail Yumanov <yumanov_m@mail.ru>
2020-11-24 17:03:38 +0300

    Added Node::getLink

--- jsyntrax/src/main/java/org/atpfivt/jsyntrax/units/nodes/Node.java
+++ jsyntrax/src/main/java/org/atpfivt/jsyntrax/units/nodes/Node.java
@@ -4,17 +4,25 @@ import org.atpfivt.jsyntrax.styles.Style;
 import org.atpfivt.jsyntrax.units.Unit;
 import org.atpfivt.jsyntrax.visitors.Visitor;
 
+import java.net.URL;
+
 public class Node implements Unit {
   Style style;
   String text;
+  URL link;
 
   public Node(String text) {
     this.text = text;
   }
 
-  public Node(Style style, String text) {
+  public Node(Style style, String text, URL link) {
     this.style = style;
     this.text = text;
+    this.link = link;
+  }
+
+  public URL getLink() {
+    return link;
   }
 
   @Override
