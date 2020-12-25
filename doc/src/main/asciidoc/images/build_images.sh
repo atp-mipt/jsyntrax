3ce76cb5cfbf18aae644b8ac6926ee6415d688b2
inponomarev <iponomarev@mail.ru>
2020-12-25 19:18:59 +0300

    documentation update

--- doc/src/main/asciidoc/images/build_images.sh
+++ doc/src/main/asciidoc/images/build_images.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-OPTS=-t
+OPTS='-t -s syntrax.ini'
 
 #syntrax -i syntax_menu.spec -o syntax_menu.svg $OPTS
 
 for f in *.spec; do
   dest=${f%.*}.svg
   syntrax -i $f -o $dest $OPTS
-  #echo $f ${f%.*}.svg
+  echo $f ${f%.*}.svg
 done
 
