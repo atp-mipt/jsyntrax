52ca2a4866f814d23b90b6deb9f1965efa9f87ac
Alexander Kirilchenko <kirilchenko.aa@phystech.edu>
2021-04-06 10:20:52 +0300

    FIX FileApprover customized to compare files as images

--- jsyntrax/src/test/java/org/atpfivt/jsyntrax/SVGTranscoderTest.java
+++ jsyntrax/src/test/java/org/atpfivt/jsyntrax/SVGTranscoderTest.java
@@ -1,31 +1,34 @@
 package org.atpfivt.jsyntrax;
 
-import org.apache.batik.transcoder.TranscoderException;
 import org.approvaltests.Approvals;
+import org.approvaltests.approvers.FileApprover;
+import org.approvaltests.core.Options;
+import org.approvaltests.core.VerifyResult;
+import org.approvaltests.writers.FileApprovalWriter;
 import org.atpfivt.jsyntrax.generators.SVGCanvas;
 import org.atpfivt.jsyntrax.generators.SVGCanvasBuilder;
 import org.atpfivt.jsyntrax.groovy_parser.Parser;
 import org.atpfivt.jsyntrax.styles.StyleConfig;
 import org.atpfivt.jsyntrax.units.Unit;
-import org.atpfivt.jsyntrax.util.SVGTranscoder;
 import org.junit.jupiter.api.Test;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
 import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Base64;
+import java.util.Arrays;
 
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 public class SVGTranscoderTest {
     @Test
     public void transcodeTest() throws IllegalAccessException, NoSuchFieldException,
-            IOException, URISyntaxException, TranscoderException {
+            IOException, URISyntaxException {
         // Given
         String config  = Files.readString(
                 Path.of(this
@@ -56,12 +59,38 @@ public class SVGTranscoderTest {
 
         //Then
         Path inputPath = Paths.get(MainTest.class.getResource("test_spec.txt").toURI());
+        Path configPath = Paths.get(MainTest.class.getResource("test_style_config.ini").toURI());
         Path outPath = Files.createTempFile("jsyntrax-test-output", ".png");
+
+        FileApprover binaryFileApprover = new FileApprover(
+                new FileApprovalWriter(outPath.toFile()),
+                new Options().forFile().getNamer(),
+                this::imageApprover
+        );
         try {
-            Main.main("-o", outPath.toString(), inputPath.toString());
-            Approvals.verify(outPath.toFile());
+            Main.main("-o", outPath.toString(), inputPath.toString(), "-s", configPath.toString());
+            Approvals.verify(binaryFileApprover);
         } finally {
             Files.deleteIfExists(outPath);
         }
+    }
+
+    private VerifyResult imageApprover(File received, File approved) {
+        boolean success = Arrays.equals(
+                getImageBytes(received),
+                getImageBytes(approved)
+        );
+        return VerifyResult.from(success);
+    }
+
+    public byte[] getImageBytes(File input) {
+        byte[] imageBytes = null;
+        try {
+            BufferedImage img = ImageIO.read(input);
+            imageBytes = ((DataBufferByte) img.getRaster().getDataBuffer()).getData();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        return imageBytes;
     }
 }
