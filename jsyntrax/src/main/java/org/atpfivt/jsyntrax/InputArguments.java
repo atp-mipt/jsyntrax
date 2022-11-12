07da6f1f08345b5dd2a52248677ff8d0f4a35dbc
Ivan Ponomarev <iponomarev@mail.ru>
2022-11-12 03:19:06 +0200

    update groovy version

--- jsyntrax/src/main/java/org/atpfivt/jsyntrax/InputArguments.java
+++ jsyntrax/src/main/java/org/atpfivt/jsyntrax/InputArguments.java
@@ -1,11 +1,11 @@
 package org.atpfivt.jsyntrax;
 
-import groovyjarjarcommonscli.CommandLine;
-import groovyjarjarcommonscli.HelpFormatter;
-import groovyjarjarcommonscli.Option;
-import groovyjarjarcommonscli.Options;
-import groovyjarjarcommonscli.ParseException;
-import groovyjarjarcommonscli.PosixParser;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.commons.cli.PosixParser;
 
 import java.io.PrintWriter;
 import java.nio.file.Path;
